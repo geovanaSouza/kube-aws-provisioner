dcf887925f8b5965c9a85ef0241e427ba529d072
geovanaSouza <geovana.possenti@gmail.com>
2017-01-16 01:03:59 -0200

    Change validation order

--- scripts/003-cluster-provisioner.sh
+++ scripts/003-cluster-provisioner.sh
@@ -2,7 +2,15 @@
 
 source scripts/.set-env.sh
 
-cd ${CLUSTER_NAME}
-kube-aws up
-aws ec2 create-volume --availability-zone ${AWS_REGION} --size ${MQ_CAPACITY} --volume-type gp2
+cd k8s_${CLUSTER_NAME}
+kube-aws render
+kube-aws validate --s3-uri s3://${AWS_BUCKET}
+
+kube-aws status 2> /dev/null
+if [ $? -ne 0 ];then
+  kube-aws up --s3-uri s3://${AWS_BUCKET}
+fi
+
+kube-aws status > ../info/kube.txt
+#aws ec2 create-volume --availability-zone ${AWS_REGION} --size ${MQ_CAPACITY} --volume-type gp2
 
