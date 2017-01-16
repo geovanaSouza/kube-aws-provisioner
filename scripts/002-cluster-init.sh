3dbdc39a9770d0c3dd199ed4593684f21b6247bd
geovanaSouza <geovana.possenti@gmail.com>
2017-01-16 01:03:17 -0200

    Add validations

--- scripts/002-cluster-init.sh
+++ scripts/002-cluster-init.sh
@@ -1,20 +1,50 @@
 #!/bin/bash
 
 source scripts/.set-env.sh
+if [ -f info/.kms_key ];then
+  source info/.kms_key
+fi
 
-aws kms --region=${AWS_REGION} --profile ${AWS_PROFILE} create-key --description="kube-aws assets"
+create_kms(){
+  status_key=$(aws kms --region=${AWS_REGION} --profile=${AWS_PROFILE} describe-key --key-id ${KUBE_KMS} | jq .KeyMetadata.KeyState | tr -d '"' 2> /dev/null)
+  if [[ ${status_key} != 'Enabled' ]];then
+    export KUBE_KMS=$(aws kms --region=${AWS_REGION} --profile ${AWS_PROFILE} create-key --description="${CLUSTER_NAME} kube-aws key" --output json --query 'KeyMetadata.KeyId' | tr -d '"')
+    export KUBE_ARN=$(aws kms --region=${AWS_REGION} --profile=${AWS_PROFILE} describe-key --key-id ${KUBE_KMS} | jq .KeyMetadata.Arn)
+    echo "export KUBE_KMS=${KUBE_KMS}" > info/.kms_key
+    echo "export KUBE_ARN=${KUBE_ARN}" >> info/.kms_key
+    echo KMS Key created: ${KUBE_KMS}
+  else
+    echo Key already exist. Using ${KUBE_ARN}
+  fi
+}
 
-kube-aws init \
---cluster-name=${CLUSTER_NAME} \
---external-dns-name=${EXTERNAL_DNS} \
---region=${AWS_REGION} \
---availability-zone=${AWS_ZONE} \
---key-name=${AWS_KEY} \
---kms-key-arn=${AWS_ARN}
+create_bucket(){
+  status_bucket=$(aws s3  ls | grep ${AWS_BUCKET} | awk '{print $3}')
+  if [[ ${status_bucket} != ${AWS_BUCKET} ]];then
+    aws s3api create-bucket --bucket ${AWS_BUCKET} --region ${AWS_REGION} --create-bucket-configuration LocationConstraint=${AWS_REGION}
+  else
+    echo Bucket already exist. Using ${AWS_BUCKET}
+  fi
+}
 
-mkdir -p ${CLUSTER_NAME}
-mv cluster.yaml ${CLUSTER_NAME}/
+cluster_init(){
+  if [ ! -d ${CLUSTER_NAME} ];then
+    kube-aws init \
+      --cluster-name=${CLUSTER_NAME} \
+      --external-dns-name=${EXTERNAL_DNS} \
+      --region=${AWS_REGION} \
+      --availability-zone=${AWS_ZONE} \
+      --key-name=${AWS_KEY} \
+      --kms-key-arn=${KUBE_ARN}
+    
+    mkdir -p k8s_${CLUSTER_NAME}
+    mv cluster.yaml k8s_${CLUSTER_NAME}/
+    
+  else
+    echo Cluster already was initialized!
+  fi
+}
 
-cd ${CLUSTER_NAME}
-kube-aws render
-kube-aws validate
+create_kms
+create_bucket
+cluster_init
